69cf1b28686991c3ccdd37be6f345d47c05de9b0
Dimitri Zuave <dimitrizuave14@gmail.com>
2020-02-19 18:05:06 -0300

    testing some things

--- timer.py
+++ timer.py
@@ -6,24 +6,29 @@ class PyScreen():
 	sg.theme('LightBrown5')
 
 	def __init__(self):
-		# layout
-		layout = [
+		# first layout
+		default_layout = [
 			[sg.Radio('Restart','Time', key='restart'), sg.Radio('Turn off','Time', key='turnoff'), sg.Radio('Timer','Time', key='timer')],
+			[sg.Ok(size=(7, 0)), sg.Cancel(size=(7, 0))],
+			[sg.Output(size=(30, 20))]
+		]
+		timer_layout = [
 			[sg.Text('Time', size=(5, 0)), sg.Input(size=(16, 0), key='shutdowntime')],
-			[sg.Slider(range=(0, 1000), default_value=0, orientation='h', size=(15, 20), key='slider')],
-			[sg.Button('Ok', size=(7, 0)), sg.Button('Cancel', size=(7, 0))],
-			[sg.Output(size=(20, 2))]
+			[sg.Slider(range=(0, 1000), default_value=0, orientation='h', size=(15, 20), key='slider')]
 		]
 		
 		# Window
-		self.window = sg.Window('Sleepy').layout(layout)
+		self.window = sg.Window('Sleepy').layout(default_layout)
 
 		# Take time from the screen:
 
 	def Start(self):
 		while True:
 			self.button, self.values = self.window.Read()
-			print(self.values)
+			timer = self.values['timer']
+			if timer:
+				pass
+			print(self.values, timer)
 
 screen = PyScreen()
 screen.Start()
