952e8ac948bffdef3203b3b45a69d6554e26e834
Dimitri Zuave <dimitrizuave14@gmail.com>
2020-02-19 16:46:04 -0300

    output log added, window stay oppend

--- timer.py
+++ timer.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+import subprocess
 
 class PyScreen():
 
@@ -7,17 +8,22 @@ class PyScreen():
 	def __init__(self):
 		# layout
 		layout = [
-			[sg.Text('Time', size=(5, 0)), sg.Input(size=(16, 0))],
-			[sg.Button('Ok', size=(7, 0))]
+			[sg.Radio('Restart','Time', key='restart'), sg.Radio('Turn off','Time', key='turnoff'), sg.Radio('Timer','Time', key='timer')],
+			[sg.Text('Time', size=(5, 0)), sg.Input(size=(16, 0), key='shutdowntime')],
+			[sg.Slider(range=(0, 1000), default_value=0, orientation='h', size=(15, 20), key='slider')],
+			[sg.Button('Ok', size=(7, 0)), sg.Button('Cancel', size=(7, 0))],
+			[sg.Output(size=(20, 2))]
 		]
 		
 		# Window
-		window = sg.Window('Sleepy').layout(layout)
+		self.window = sg.Window('Sleepy').layout(layout)
 
 		# Take time from the screen:
 
 	def Start(self):
-		print(self.values)
+		while True:
+			self.button, self.values = self.window.Read()
+			print(self.values)
 
 screen = PyScreen()
 screen.Start()
