59591b3b4a8f1d4797aa5e038cf9fc8d941d919b
Dimitri Zuave <dimitrizuave14@gmail.com>
2020-02-20 15:42:44 -0300

    timer radio removed

--- timer.py
+++ timer.py
@@ -8,7 +8,7 @@ class PyScreen():
 	def __init__(self):
 		# first layout
 		default_layout = [
-			[sg.Radio('Restart','Time', key='restart'), sg.Radio('Turn off','Time', key='turnoff'), sg.Radio('Timer','Time', key='timer')],
+			[sg.Radio('Restart','Time', key='restart'), sg.Radio('Turn off','Time', key='turnoff')],
 			[sg.Text('Time', size=(5, 0)), sg.Input(size=(10, 0), key='time')],
 			[sg.Ok(size=(7, 0)), sg.Cancel(size=(7, 0))],
 		]
@@ -20,13 +20,19 @@ class PyScreen():
 		self.button, self.values = window.Read()
 
 	def Start(self):
-		timer = self.values['timer']
+		restart = self.values['restart']
+		turnoff = self.values['turnoff']
 		time = self.values['time']
-		# comm_string = f'shutdown -s -t {time}'
 		comm_string = 'dir'
-		if timer:
+		if restart:
+			# comm_string = f'shutdown -r -t {time}'
 			subprocess.run(f'{comm_string}', shell = True)
-		print(self.values, timer)
+			pass
+		if turnoff:
+			# comm_string = f'shutdown -s -t {time}'
+			subprocess.run(f'{comm_string}', shell = True)
+			
+		print(self.values)
 
 screen = PyScreen()
 screen.Start()
