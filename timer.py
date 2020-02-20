0b1ef0e6a665ee80edb048cb62e2a408bca457ed
Dimitri Zuave <dimitrizuave14@gmail.com>
2020-02-20 15:32:35 -0300

    The shutdown is working

--- timer.py
+++ timer.py
@@ -9,11 +9,8 @@ class PyScreen():
 		# first layout
 		default_layout = [
 			[sg.Radio('Restart','Time', key='restart'), sg.Radio('Turn off','Time', key='turnoff'), sg.Radio('Timer','Time', key='timer')],
+			[sg.Text('Time', size=(5, 0)), sg.Input(size=(10, 0), key='time')],
 			[sg.Ok(size=(7, 0)), sg.Cancel(size=(7, 0))],
-		]
-		timer_layout = [
-			[sg.Text('Time', size=(5, 0)), sg.Input(size=(16, 0), key='shutdowntime')],
-			[sg.Slider(range=(0, 1000), default_value=0, orientation='h', size=(15, 20), key='slider')]
 		]
 		
 		# Window
@@ -24,6 +21,11 @@ class PyScreen():
 
 	def Start(self):
 		timer = self.values['timer']
+		time = self.values['time']
+		# comm_string = f'shutdown -s -t {time}'
+		comm_string = 'dir'
+		if timer:
+			subprocess.run(f'{comm_string}', shell = True)
 		print(self.values, timer)
 
 screen = PyScreen()
